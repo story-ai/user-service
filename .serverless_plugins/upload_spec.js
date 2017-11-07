b1182f3c78882fdf4f7660f5038e6c1177034f0a
Christopher Little <christopher@littlehq.uk>
2017-11-07 18:20:17 +0000

    Add documentation for the /spec endpoint

--- .serverless_plugins/upload_spec.js
+++ .serverless_plugins/upload_spec.js
@@ -4,7 +4,9 @@ const afterUpdate = (serverless, options) => {
 	const stage = options.stage || serverless.service.provider.stage;
 	const region = options.region || serverless.service.provider.region;
 	const aws = serverless.getProvider("aws");
-	const apiName = aws.naming.getApiGatewayName();
+	const apiName =
+		serverless.service.resources.Resources.ApiGatewayRestApi.Properties
+			.Name;
 
 	serverless.cli.log("Fetching API Gateway ID...");
 	return aws
@@ -12,6 +14,7 @@ const afterUpdate = (serverless, options) => {
 			limit: 500,
 		})
 		.then(apis => {
+			console.log("Seeking ", apiName);
 			const api = apis.items.find(entry => entry.name === apiName);
 			if (api !== undefined) {
 				return api.id;
@@ -29,6 +32,17 @@ const afterUpdate = (serverless, options) => {
 		})
 		.then(result => {
 			serverless.cli.log("Uploading spec to S3...");
+			const spec = JSON.parse(result.body);
+
+			// remove OPTIONS endpoints - these don't need documenting
+			for (let path in spec.paths) {
+				if (spec.paths[path].options) delete spec.paths[path].options;
+			}
+
+			// add description for the spec endpoint
+			// TODO: I want this in the yaml, but if I put it in docs.yaml, it gets ignored; if I put it in as a CloudFormation resource, it gets overwritten by the serverless docs plugin
+			console.log(spec.paths["/spec"]);
+			spec.paths["/spec"].get.summary = "Retrieve this API specification";
 
 			return aws.request(
 				"S3",
@@ -36,7 +50,7 @@ const afterUpdate = (serverless, options) => {
 				{
 					ACL: "public-read",
 					Bucket: serverless.service.custom.spec_bucket,
-					Body: result.body,
+					Body: JSON.stringify(spec),
 					ContentType: "application/json",
 					Key: serverless.service.service + ".json",
 				},
